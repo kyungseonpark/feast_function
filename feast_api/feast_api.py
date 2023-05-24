f34494db9887f4abcecb98919774a49bff5bcf97
kyungseonpark <kyungseon.park@aizen.co>
2023-05-24 12:48:11 +0900

    second commit

--- feast_api/feast_api.py
+++ feast_api/feast_api.py
@@ -3,9 +3,9 @@ import pickle
 import subprocess
 
 import pandas
-from functools import wraps
 
-from fastapi import status
+
+
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import UploadFile, File, Form
@@ -15,7 +15,7 @@ from fastapi.exceptions import RequestValidationError
 
 from feast_function import FeastRepo, FeastDataset, FeastServe
 from feast_function import FeastRepoItem, FeastDatasetItem, FeastServeItem
-from feast_function import feast_push_server_boot
+from feast_function import feast_push_server_boot, exception_handler, check_api_body
 from feast_global import *
 
 feast = FastAPI(
@@ -36,61 +36,12 @@ async def startup_event():
     feast_push_server_boot()
 
 
-
-
-
 @feast.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc):
     return JSONResponse(
         status_code=400,
         content=jsonable_encoder(exc.errors())
     )
-
-
-def exception_handler(func):
-    """
-    Decorator created for debugging when the feast-server encounters an [Internal Server Error].
-    feast-server에서 error가 발생했을때 내부에러라고만 출력되어서 Debugging용 데코레이터.
-    """
-    @wraps(func)
-    def execute_func(*args, **kwargs):
-        try:
-            res = func(*args, **kwargs)
-            return res
-        except HTTPException as e:
-            return JSONResponse(
-                status_code=500,
-                content=jsonable_encoder({
-                    "loc": f'{func.__name__}',
-                    "exception": "HTTPException",
-                    "msg": f'{e.detail}'
-                })
-            )
-        except Exception as e:
-            return JSONResponse(
-                status_code=500,
-                content=jsonable_encoder({
-                    "loc": f'{func.__name__}',
-                    "exception": "Exception",
-                    "msg": f'{e}'
-                })
-            )
-    return execute_func
-
-
-def check_api_body(api_body: dict, need_body: list):
-    """
-    Server핸들링 Request Body 클래스가 Optional로 지저분해져서 Body를 체크해서 필수항목을 확인하기위한 함수.
-    :param api_body: 실제로 입력으로 들어온 Body parameter
-    :param need_body: 필수로 들어와야하는 Body parameter
-    """
-    res_list = list()
-    for item in need_body:
-        if api_body[item] is None:
-            res_list.append(item)
-    if len(res_list) != 0:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f'Not Found Item, {res_list}')
-
 
 @exception_handler
 @feast.post("/init/", tags=[TAGS[0]])
